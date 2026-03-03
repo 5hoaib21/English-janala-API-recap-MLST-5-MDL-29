795aca1d7714170b3e0e4817139ec1c3ccf54141
MD ABDUR RAHMAN SHOAIB <mdabdurrahmanshoaib@gmail.com>
2026-03-04 03:12:43 +0600

    Handle Errors , empty lessons for Better UX

--- scripts/index.js
+++ scripts/index.js
@@ -12,9 +12,27 @@ const loadLevelWord = (id) => {
     .then((res) => res.json())
     .then((data) => displayLevelWord(data.data));
 };
+
+
 const displayLevelWord = (words) => {
   const wordContainer = document.getElementById("word-container");
   wordContainer.innerHTML = "";
+
+
+  if(words.length==0){
+     wordContainer.innerHTML = `
+     <div class="text-center col-span-full space-y-6">
+     <img class="mx-auto" src="./assets/alert-error.png" alt="">
+     <p class="text-xl opacity-50 font-bangla">
+     এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।
+     </p>
+     <h2 class="text-5xl font-medium font-bangla">
+     নেক্সট Lesson এ যান
+     </h2>
+      </div>
+     `;
+    return;
+  }
 
 //   {
 //     "id": 79,
@@ -31,9 +49,9 @@ const displayLevelWord = (words) => {
 <div
         class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4"
       >
-        <h2 class="font-bold text-2xl">${word.word}</h2>
+        <h2 class="font-bold text-2xl">${word.word ? word.word:"শব্দ পাওয়া যায়নি"}</h2>
         <p class="font-semibold">Meaning /Pronounciation</p>
-        <div class="text-2xl font-medium font-bangla">"${word.meaning} / ${word.pronunciation}"</div>
+        <div class="text-2xl font-medium font-bangla">"${word.meaning ? word.meaning:"অর্থ পাওয়া যায়নি"} / ${word.pronunciation ? word.pronunciation:"Pronounciation পাওয়া যায়নি"}"</div>
         <div class="flex justify-between items-center">
           <button class="btn bg-blue-50 hover:bg-blue-300 rounded-lg">
             <i class="fa-solid fa-circle-info"></i>
