cc94b110a6e8c2ef1fbab2284c6da04d5c9b3c62
MD ABDUR RAHMAN SHOAIB <mdabdurrahmanshoaib@gmail.com>
2026-03-04 07:48:33 +0600

    delete extra file

--- test.js
+++
@@ -1,7 +0,0 @@
-const createEls = (arr) => {
-  const htmlEls = arr.map((el) => `<span class="btn">${el}</span>`);
-  console.log(htmlEls.join(" "));
-};
-
-const synonyms = ["hello", "hi", "shoaib"];
-createEls(synonyms);
